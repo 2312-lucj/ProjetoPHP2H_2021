9494d7461488aec7796ec08b49f9b20058a977c2
Lucas <lucas.oliva2306@gmail.com>
2021-11-04 22:10:46 -0300

    Aula do dia 04/11

---
+++ pagina_elemento_checkbox_isset.php
@@ -0,0 +1,18 @@
+<?php
+
+   echo "<h3>Recuperando od dados - Elemento checkbox - ISSET</h3>";
+
+   if (isset($_POST['asp'])){
+   	echo $_POST['asp'];
+    echo "<br>";
+   }
+   if (isset($_POST['php'])){
+   	echo $_POST['php'];
+    echo "<br>";
+   }
+   if (isset($_POST['java'])){
+   	echo $_POST['java'];
+    echo "<br>";
+   }
+
+  ?> 
